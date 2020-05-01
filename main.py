cc6e93b13da61c53ebc4d67d2a51b27efdc2a602
swagash <59715831+SwagAsh@users.noreply.github.com>
2020-05-01 15:12:10 -0400

    moving away from repl

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import discord
 import os
-import flask
-from keep_alive import keep_alive
 from discord.ext import commands
 
 earthchan = commands.Bot(command_prefix = ';')
 
-token = os.environ.get("DISCORD_BOT_SECRET")
+t = open("token.txt", "r")
 
 @earthchan.command()
 async def load(ctx, extension):
@@ -20,6 +18,4 @@ for filename in os.listdir('./categories'):
     if filename.endswith('.py'):
         earthchan.load_extension(f'categories.{filename[:-3]}')
 
-keep_alive()
-token = os.environ.get("DISCORD_BOT_SECRET")
-earthchan.run(token)
+earthchan.run(t.read())
